f55868944a40d3e3457dfa68b27fc7b08d57454c
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-03-22 14:56:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!' to contain 'Grace Hopper';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// 'Edsger Dijkstra'.slice(0, 3);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
